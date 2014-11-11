6422bb0446b0b3ed4179c6348d025ff68dda4045
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-11-11 14:21:27 -0200

    avocado.virt.qemu.machine: Use short_id for serial console filename
    We are using short_id to identify vms now, so it makes
    sense to name the serial console file accordingly.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- avocado/virt/qemu/machine.py
+++ avocado/virt/qemu/machine.py
@@ -86,7 +86,7 @@ class VM(object):
                 "nc -U %s" % self.serial_socket,
                 auto_close=False,
                 output_func=io.log_line,
-                output_params=("serial-console-%#x.log" % id(self),),
+                output_params=("serial-console-%s.log" % self.short_id,),
                 prompt=self.params.get("shell_prompt", "[\#\$]"))
             self._screendump_thread.start()
         finally:
